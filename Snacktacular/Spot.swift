bbcc7da19ca8f6363a719389a6e10c121ac7eb66
Morgan Glover <glovermo@bc.edu>
2019-11-04 11:32:07 -0500

    9.7 Completed - Add MapView

--- Snacktacular/Spot.swift
+++ Snacktacular/Spot.swift
@@ -9,8 +9,9 @@
 import Foundation
 import CoreLocation
 import Firebase
+import MapKit
 
-class Spot {
+class Spot: NSObject, MKAnnotation {
     var name: String
     var address: String
     var coordinate: CLLocationCoordinate2D
@@ -24,6 +25,14 @@ class Spot {
     }
     var longitude: CLLocationDegrees {
         return coordinate.longitude
+    }
+    
+    var title: String? {
+        return name
+    }
+    
+    var subtitle: String? {
+        return address
     }
     
     var dictionary: [String: Any] {
@@ -40,7 +49,7 @@ class Spot {
         self.documentID = documentID
     }
     
-    convenience init() {
+    override convenience init() {
         self.init(name: "", address: "", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: "")
     }
     
